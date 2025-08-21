2c8ce61a330a96d4128e507f6b5f241838b8cdf7
Cynthia Cetraro <ccetraro.85@gmail.com>
2025-08-21 18:59:52 -0500

    feat: mostrar amigos en la lista HTML

--- app.js
+++ app.js
@@ -21,5 +21,24 @@ function agregarAmigo(){
     document.getElementById("amigo").value = "";
 
     console.log(amigos);
+    //Agrego la 2da función al final de la 1ra para que dentro de la acción del botón se imprima la lista. 
+    agregarAmigoImprimeLista();
+}
+
+/*Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML. 
+Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.*/
+
+function agregarAmigoImprimeLista(){
+//  1.Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
+    let lista = document.getElementById("listaAmigos");
+//  2.Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
+    lista.innerHTML = "";
+//  3.Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
+    for (let i = 0; i < amigos.length; i++){
+        let bullet = document.createElement("li");
+//  4.Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
+        bullet.textContent = amigos[i];
+        lista.appendChild(bullet);
+        }
 }
 
